e827d7b24412b374536d214282513f2cd09546e1
ard71 <ard71@pitt.edu>
2017-03-01 18:49:10 -0500

    got rid of unnecessary php file

--- js/contact.js
+++ js/contact.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
 	$("#submit").click(function(){
-		var name = $("#name").val();
-		var email = $("#email").val();
-		var phone = $("#phone").val();
-		var message = $("#message").val();
+		var name1 = $("#name").val();
+		var email1 = $("#email").val();
+		var phone1 = $("#phone").val();
+		var message1 = $("#message").val();
 		
 		$("#returnmessage").empty();
 		
-		if (name == '' || email == '' || message == ''){
-			alert("Please fill all required fileds.");
+		if (name1 == '' || email1 == '' || message1 == ''){
+			alert("Please fill all required fields.");
 		}
 		else{
-			$.post("contact.php",{
-				name1: name,
-				email1: email,
-				message1: message,
-				phone1: phone
+			$.post("form-handler-nodb.php",{
+				name: name1,
+				email: email1,
+				message: message1,
+				phone: phone1
 			}, function(data) {
 				$("#returnmessage").append(data);
 				if (data == "Message sent successfully."){
